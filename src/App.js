c3a55796137fbe1d80428d173381006ac5bc32b8
mzaeemnasir <m.zaeem.nasir@gmail.com>
2023-05-26 02:10:13 +0500

    update

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import backgroundImage from './image/q.jpg';
 
 function CustomIndicator() {
   const [conditions, setConditions] = useState([{ indicator: '', length: '', condition: '', value: '' }]);
-  const [takeProfit, setTakeProfit] = useState(false);
-  const [stopLoss, setStopLoss] = useState(false);
+  const [takeProfit, setTakeProfit] = useState("");
+  const [stopLoss, setStopLoss] = useState("");
+  const [binance_api, setBinance_api] = useState('');
+  const [binance_secret, setBinance_secret] = useState('');
+  const [discord_webhook, setDiscord_webhook] = useState('');
 
   const indicators = ['PriceOpen', 'PriceHigh', 'PriceLow', 'PriceClose', "MACD 12,26", "EMA", "SMA", "RSI"];
   const conditionsList = ['Greater Than Or Equal', 'Less Than Or Equal', 'Crosses Above', 'Crosses Below', 'Equal'];
@@ -12,7 +15,7 @@ function CustomIndicator() {
 
 
   const handleAddCondition = () => {
-    setConditions([...conditions, { indicator: '', length: '', condition: '', value: '', valueLength: '' }]);
+    setConditions([...conditions, { indicator: '', length: '', condition: '', choice: "", indicator2: '', length2: '', constantValue: '' }]);
   };
 
   const handleRemoveCondition = (index) => {
@@ -31,16 +34,19 @@ function CustomIndicator() {
     setConditions(newConditions);
   };
 
-  const handleTakeProfitChange = (event) => {
-    setTakeProfit(event.target.checked);
-  };
-
-  const handleStopLossChange = (event) => {
-    setStopLoss(event.target.checked);
-  };
 
   const handleSubmit = () => {
+
+    if (binance_api === '' || binance_secret === '' || discord_webhook === '' || takeProfit === '' || stopLoss === '') {
+      alert('Please fill all the fields');
+      return;
+    }
+
+
     const data = {
+      binance_api,
+      binance_secret,
+      discord_webhook,
       conditions,
       takeProfit,
       stopLoss,
@@ -50,10 +56,9 @@ function CustomIndicator() {
     alert('JSON data: ' + jsonData);
 
     // Clear input fields
-    setConditions([{ indicator: '', length: '', condition: '', value: '' }]);
-    setTakeProfit(false);
-    setStopLoss(false);
+    setConditions([{ indicator: '', length: '', condition: '', choice: "", indicator2: '', length2: '', constantValue: '' }]);
   };
+
 
   return (
     <div
@@ -68,7 +73,7 @@ function CustomIndicator() {
         minHeight: '100vh',
       }}
     >
-      <h1 style={{ color: '#f5f5f5', marginBottom: '100px', marginTop: '200px' }}>Create your own Custom Trading Bot</h1>
+      <h1 style={{ color: '#f5f5f5', marginBottom: '100px', marginTop: '100px' }}>Create your own Custom Trading Bot</h1>
       {conditions.map((condition, index) => (
         <div key={index}>
           <select
@@ -90,7 +95,7 @@ function CustomIndicator() {
               name="length"
               value={condition.length}
               onChange={(event) => handleConditionChange(index, event)}
-              placeholder="Length"
+              placeholder="Indicator 1 Length"
               style={{ backgroundColor: '#fff', padding: '5px', borderRadius: '3px', border: '1px solid #ccc', marginRight: '10px' }}
             />
           ) : null}
@@ -123,18 +128,18 @@ function CustomIndicator() {
           {condition.choice === "ConstantValue" ? (
             <input
               type="text"
-              name="value"
-              value={condition.value}
+              name="constantValue"
+              value={condition.constantValue}
               onChange={(event) => handleConditionChange(index, event)}
-              placeholder="Value"
+              placeholder="Constant Value"
               style={{ backgroundColor: '#fff', padding: '5px', borderRadius: '3px', border: '1px solid #ccc', marginRight: '10px' }}
             />
           ) : null}
 
           {condition.choice === "Indicator" ? (
             <select
-              name="value"
-              value={condition.value}
+              name="indicator2"
+              value={condition.indicator2}
               onChange={(event) => handleConditionChange(index, event)}
               style={{ backgroundColor: '#fff', padding: '5px', borderRadius: '3px', border: '1px solid #ccc', marginRight: '10px' }}
             >
@@ -146,16 +151,16 @@ function CustomIndicator() {
               ))}
             </select>
 
-
-
           ) : null}
-          {condition.value.toString().startsWith("Price") === false && condition.value.toString().startsWith("MACD") === false ? (
+          {condition.indicator2 && condition.indicator2.toString().startsWith("Price") === false && condition.indicator2.toString().startsWith("MACD") === false
+            && condition.choice.startsWith("Constant") === false ? (
+
             <input
               type="text"
-              name="valueLength"
+              name="length2"
               value={condition.valueLength}
               onChange={(event) => handleConditionChange(index, event)}
-              placeholder="Length"
+              placeholder="Indicator 2 Length"
               style={{ backgroundColor: '#fff', padding: '5px', borderRadius: '3px', border: '1px solid #ccc', marginRight: '10px' }}
             />
           ) : null}
@@ -176,17 +181,13 @@ function CustomIndicator() {
       </button>
       <div>
         <label style={{ color: '#ffffff', margin: '10px' }}>
-          <input
-            type="checkbox"
-            checked={takeProfit}
-            onChange={handleTakeProfitChange}
-          />
           Take Profit
-        </label>
-        {takeProfit ? (
+
           <input
             type="text"
-            placeholder="Exit if user gained X%"
+            onChange={(event) => setTakeProfit(event.target.value)}
+            placeholder="Exit if Gained X%"
+            value={takeProfit}
             style={{
               backgroundColor: '#ffffff',
               padding: '5px',
@@ -195,29 +196,132 @@ function CustomIndicator() {
               marginLeft: '10px',
             }}
           />
-        ) : null}
+        </label>
       </div>
       <div>
-        <label style={{ color: '#ffffff', margin: '13px' }}>
-          <input
-            type="checkbox"
-            checked={stopLoss}
-            onChange={handleStopLossChange}
-          />
+        <label style={{ color: '#ffffff', margin: '10px' }}>
           Stop Loss
-        </label>
-        {stopLoss ? (
+
           <input
             type="text"
-            placeholder="Exit if loss more than X%"
-            style={{ backgroundColor: '#ffffff', padding: '5px', borderRadius: '3px', border: '1px solid #ccc', marginLeft: '10px' }}
+            onChange={(event) => setStopLoss(event.target.value)}
+            placeholder="Exit if Lost X%"
+            value={stopLoss}
+            style={{
+              backgroundColor: '#ffffff',
+              padding: '5px',
+
+              borderRadius: '3px',
+              border: '1px solid #ccc',
+              marginLeft: '13px',
+              marginTop: '10px',
+            }}
           />
-        ) : null}
+        </label>
       </div>
+
+
+      <div>
+        <label style={{ color: '#ffffff', margin: '10px' }}>
+          Binance API Key
+
+          <input
+            type="text"
+            onChange={(event) => setBinance_api(event.target.value)}
+            placeholder="Binance API KEY"
+            value={binance_api}
+            style={{
+              backgroundColor: '#ffffff',
+              padding: '5px',
+              width: '300px',
+              borderRadius: '3px',
+              border: '1px solid #ccc',
+              marginLeft: '10px',
+              marginTop: '20px',
+            }}
+          />
+        </label>
+      </div>
+
+
+      <div>
+        <label style={{ color: '#ffffff', margin: '10px' }}>
+          Binance API Secret
+
+          <input
+            type="password"
+            placeholder="Binance API Secret"
+            value={binance_secret}
+            onChange={(event) => setBinance_secret(event.target.value)}
+            style={{
+              backgroundColor: '#ffffff',
+              padding: '5px',
+              borderRadius: '3px',
+              width: '320px',
+
+              border: '1px solid #ccc',
+              marginLeft: '13px',
+              marginTop: '10px',
+            }}
+          />
+        </label>
+      </div>
+
+
+
+
+      <div>
+        <label style={{ color: '#ffffff', margin: '10px' }}>
+          Discord Webhook
+
+          <input
+            type="text"
+            value={discord_webhook}
+            placeholder="Discord Webhook"
+            onChange={(event) => setDiscord_webhook(event.target.value)}
+            style={{
+              backgroundColor: '#ffffff',
+              padding: '5px',
+              borderRadius: '3px',
+              width: '320px',
+              border: '1px solid #ccc',
+              marginLeft: '13px',
+              marginTop: '10px',
+            }}
+          />
+        </label>
+      </div>
+
+
+
       <button onClick={handleSubmit} style={{ backgroundColor: 'blue', color: '#fff', border: 'none', padding: '10px 20px', marginTop: '30px', borderRadius: '3px' }}>
         Submit
       </button>
+
+      <div>
+        <h1 style={{ color: "white" }}>Disclaimer:</h1>
+        <p style={{ color: '#ffffff', margin: '10px', marginTop: '50px' }}>
+          Please note that the following information is provided for educational and informational purposes only. Trading strategies involve risks, and it is essential to exercise caution and conduct thorough research before engaging in any trading activities. The disclaimer below can be used as a starting point for your website, but it's recommended to consult with legal professionals to ensure compliance with applicable laws and regulations.
+          <br /><br />Trading involves significant risks, and past performance is not indicative of future results. The strategies described on this website may not be suitable for all individuals, and there is no guarantee of profitability.
+
+          <br /><br />The use of leverage amplifies both potential gains and losses. Trading with leverage involves a higher level of risk, and you should carefully consider your risk tolerance and financial situation before engaging in leveraged trading.
+
+          <br /><br />This strategy is designed specifically for "Long Position" trading on the BTCUSDT.P ticker using 3x leverage, with the inclusion of both take-profit and stop-loss orders. It may not be suitable for other trading pairs, leverages, or trading directions. Always ensure that you understand the implications and risks associated with your trading decisions.
+
+          <br /><br />The trading strategies implemented on this platform are solely the responsibility of the user. We do not guarantee the accuracy, completeness, or effectiveness of any strategy, and we disclaim any liability for losses incurred due to the use of these strategies.
+
+          <br /><br />It is your responsibility to thoroughly test and validate any strategies before applying them to your Binance account. We recommend starting with small amounts and gradually increasing your exposure as you gain confidence and experience.
+
+          <br /><br />By connecting your Binance account to this platform, you understand and accept that you are granting necessary permissions for the automation of your trading activities. You should review and understand Binance's terms of service and privacy policy before proceeding.
+
+          <br /><br />We strongly advise you to consult with a qualified financial advisor or professional before making any investment decisions. They can provide personalized advice based on your specific financial situation and risk tolerance.
+
+        </p>
+
+      </div>
+
     </div >
+
   );
 }
 
