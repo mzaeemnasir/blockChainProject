4b70bb86f9b10a0b19560eeff194513a5ed261da
mzaeemnasir <m.zaeem.nasir@gmail.com>
2023-06-08 22:05:01 +0500

    update

--- Bot/parseCondition.py
+++ Bot/parseCondition.py
@@ -8,10 +8,22 @@ condition = {
       "indicator2": "PriceHigh",
       "length2": "",
       "constantValue": ""
+    },  
+    {
+      "indicator": "PriceOpen",
+      "length": "",
+      "condition": "Greater Than Or Equal",
+      "choice": "Indicator",
+      "indicator2": "PriceHigh",
+      "length2": "",
+      "constantValue": ""
     }
   ],
   "takeProfit": "123",
-  "stopLoss": "1231"
+  "stopLoss": "1231",
+  "binance_api": "123123",
+  "binance_secret": "print(e)",
+  "discord_webhook": "1231231",
 }
 
 
@@ -27,11 +39,18 @@ col = db["data"]
 
 
 def get_data():
-    data = col.find_one()
-    return data
+  data = col.find_one()
+  return data
   
 def add_data():
-    data = col.insert_one(condition)
-    return data
+  data = col.insert_one(condition)
+  return data
+  
+  
+def parsing_the_data(jsonData):
+  indicator1 = jsonData["conditions"][0]["indicator"]
+  
+  
+  
   
 print(add_data())
